3746cd7538503eb663428c80756bb846f9d021a7
Maria Saveleva <nokiae72i@yandex.ru>
2014-03-30 19:15:51 +0400

    Added map list

---
+++ EventTracker/GW2MapList.h
@@ -0,0 +1,13 @@
+//
+//  GW2MapList.h
+//  EventTracker
+//
+//  Created by Maria Saveleva on 30/03/14.
+//  Copyright (c) 2014 Maria Saveleva. All rights reserved.
+//
+
+#import "MTLModel.h"
+
+@interface GW2MapList : MTLModel
+
+@end
---
+++ EventTracker/GW2MapList.m
@@ -0,0 +1,14 @@
+
+//
+//  GW2MapList.m
+//  EventTracker
+//
+//  Created by Maria Saveleva on 30/03/14.
+//  Copyright (c) 2014 Maria Saveleva. All rights reserved.
+//
+
+#import "GW2MapList.h"
+
+@implementation GW2MapList
+
+@end
