c3d21d60e36e54b044afb951223de5a132b759b1
Maria Saveleva <mariasaveleva@Artems-Mac-Pro.local>
2014-04-11 22:34:04 +0400

    Added request for wiki page

--- EventTracker/GW2WikiViewController.m
+++ EventTracker/GW2WikiViewController.m
@@ -8,7 +8,11 @@
 
 #import "GW2WikiViewController.h"
 
+static NSString * const kGW2WikiUrl = @"http://wiki.guildwars2.com/wiki/";
+
 @interface GW2WikiViewController ()
+
+@property (weak, nonatomic) IBOutlet UIWebView *wikiWebView;
 
 @end
 
@@ -26,6 +30,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSURL *url = [NSURL URLWithString:[self getUrlString]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.wikiWebView loadRequest:request];
+}
+
+- (NSString *)getUrlString
+{
+    NSString *resultString = [self.eventName stringByReplacingOccurrencesOfString:@" " withString:@"_"];
+    if ([resultString length] > 0) {
+        resultString = [resultString substringToIndex:[resultString length] -1];
+    }
+    
+    resultString = [kGW2WikiUrl stringByAppendingString:resultString];
+    
+    return resultString;
 }
 
 @end
