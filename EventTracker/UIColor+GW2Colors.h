37695d08417477a33c2cfa5c2479db30957ebc9b
Maria Saveleva <mariasaveleva@Artems-Mac-Pro.local>
2014-04-23 09:08:00 +0400

    Added methods and category

---
+++ EventTracker/UIColor+GW2Colors.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+GW2Colors.h
+//  EventTracker
+//
+//  Created by Maria Saveleva on 23/04/14.
+//  Copyright (c) 2014 Maria Saveleva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (GW2Colors)
+
+@end
---
+++ EventTracker/UIColor+GW2Colors.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+GW2Colors.m
+//  EventTracker
+//
+//  Created by Maria Saveleva on 23/04/14.
+//  Copyright (c) 2014 Maria Saveleva. All rights reserved.
+//
+
+#import "UIColor+GW2Colors.h"
+
+@implementation UIColor (GW2Colors)
+
+@end
