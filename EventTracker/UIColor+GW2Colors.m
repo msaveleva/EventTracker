1b2efaf4dc5a65625b950643588c08a13ff5707c
Maria Saveleva <mariasaveleva@Artems-Mac-Pro.local>
2014-04-23 09:10:02 +0400

    Implemented getColorForStatus method

--- EventTracker/UIColor+GW2Colors.m
+++ EventTracker/UIColor+GW2Colors.m
@@ -10,6 +10,32 @@
 
 @implementation UIColor (GW2Colors)
 
++ (UIColor *)getColorForStatus:(GW2EventStatus)status
+{
+    switch (status) {
+        case GW2EventStatusInactive:
+            return [UIColor inactiveEventStatusLabelColor];
+            break;
+        case GW2EventStatusActive:
+            return [UIColor activeEventStatusLabelColor];
+            break;
+        case GW2EventStatusSuccess:
+            return [UIColor successEventStatusLabelColor];
+            break;
+        case GW2EventStatusFail:
+            return [UIColor failEventStatusLabelColor];
+            break;
+        case GW2EventStatusWarmup:
+            return [UIColor warmupEventStatusLabelColor];
+            break;
+        case GW2EventStatusPreparation:
+            return [UIColor preparationEventStatusLabelColor];
+            break;
+        default:
+            break;
+    }
+}
+
 + (UIColor *)activeEventStatusLabelColor
 {
     //#86c37a
